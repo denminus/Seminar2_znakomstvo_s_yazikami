fd1a0877d1f6de53e7c2fd4cc5156f5dd7e82e49
den_minus <den.minus@mail.ru>
2023-01-25 19:53:04 +0300

    Деление с остатком, ввод чисел int value = int.Parse(Console.ReadLine()!);, сравнение через &&

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Console.Write("введите число");
+//  int value = int.Parse(Console.ReadLine()!); // ! -тайное знание
+// Console.Write("You number: " + value);
+
+Console.Write("введите пятизначное число");
+int value = int.Parse(Console.ReadLine()!);
+int n1=value/10000;
+int n2=value/1000%10;
+int n3=value/100%10;
+int n4=value/10%10;
+int n5=value%10;
+if (n1==n5 && n2==n4) {
+    Console.WriteLine("ЭТО ЧИСЛО ПОЛИНДРОМ");
+}
+else {
+Console.WriteLine("ЭТО ЧИСЛО НЕ ПОЛИНДРОМ");
+}
+// Console.WriteLine(n1);
+// Console.WriteLine(n2);
+// Console.WriteLine(n3);
+// Console.WriteLine(n4);
+// Console.WriteLine(n5);
